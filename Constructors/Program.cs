569b82d4329b9209dc68911f5ff273d874571607
sevgiyilmaz <sevgiyilmaz2112@gmail.com>
2024-02-29 19:26:21 +0300

    add files

---
+++ Constructors/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+            Customer customer1 = new Customer
+            {
+                Id = 1,
+                FirstName = "Sevgi",
+                LastName = "Yılmaz",
+                City = "İst",
+            };
+
+            Customer customer2 = new Customer(2, "Tuba", "Yılmaz", "İstanbul");
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Customer
+    {
+        // Default constructor : Parametresi olmayan constructor
+        public Customer()
+        {
+            Console.WriteLine("Yapıcı blok çalıştı !");
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
--- ReferenceTypes/Program.cs
+++ ReferenceTypes/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int,decimal,float,enum,boolean -> değer tipler (value types)
+
+            int sayi1 = 10;
+            int sayi2 = 20;
+
+            sayi1 = sayi2;
+            sayi2 = 100;
+            Console.WriteLine("Sayi1 is " + sayi1);
+
+            // arraylar referans tiplerdir. arrays,classes,interface -> reference types
+            // referans tiplerde adres eşitleme yapılır.
+
+            int[] sayilar = new int[] { 1, 2, 3 };
+            int[] sayilar2 = new int[] { 10, 20, 30 };
+
+            sayilar = sayilar2;
+            sayilar2[0] = 1000;
+
+            Console.WriteLine(sayilar[0]);
+
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            person1.FirstName = "Sevgi";
+            person2 = person1;
+            person1.FirstName = "Tuba";
+            Console.WriteLine(person2.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Ali";
+            customer.CreditCardNumber = "123656544885";
+
+            Employee employee = new Employee();
+
+            // Böyle bir atama işlemi gerçekleştirilemez.
+            // customer = employee;
+
+            Person person3 = customer;
+            customer.FirstName = "Veli";
+            Console.WriteLine(person3.FirstName);
+            Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            Person person4 = employee;
+
+            PersonManager personManager = new PersonManager();
+            personManager.Add(person3);
+            // person yerine customer ya da employee gönderebiliyorum.
+            personManager.Add(customer);
+
+            Console.ReadLine();
+        }
+    }
+
+    // Base Class 
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee : Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
